2acff3773c6cd3bd466082f14cf2f210a9bc925e
Justin <justinjenecke@gmail.com>
2021-09-10 17:00:44 +0200

    makes changes to controller uri's, and changes name of app main application

--- src/main/java/za/ac/cput/ts/TsApp.java
+++ src/main/java/za/ac/cput/ts/TsApp.java
@@ -1,10 +1,11 @@
 package za.ac.cput.ts;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class App {
+public class TsApp {
     public static void main(String[] args) {
-
+        SpringApplication.run(TsApp.class, args);
     }
 }
--- src/main/java/za/ac/cput/ts/controller/PostController.java
+++ src/main/java/za/ac/cput/ts/controller/PostController.java
@@ -6,30 +6,36 @@ import org.springframework.web.bind.annotation.*;
 import za.ac.cput.ts.entity.Post;
 import za.ac.cput.ts.service.PostService;
 
+@RequestMapping("/ts/api/post")
 @RestController
 public class PostController {
 
     @Autowired
     private PostService postService;
 
-    @PostMapping("/ts/api/post/create")
+    @RequestMapping("/")
+    public String h() {
+        return "create - read{id} - update - delete{id}";
+    }
+
+    @PostMapping("/create")
     public Post createPost(@RequestBody Post post) {
         Post cp = new Post.Builder().copy(post).build();
         return this.postService.create(cp);
     }
 
-    @GetMapping("/ts/api/post/read/{id}")
+    @GetMapping("/read/{id}")
     public Post getPost(@PathVariable int id) {
         return this.postService.read(id);
     }
 
-    @PutMapping("/ts/api/post/update")
+    @PutMapping("/update")
     public Post updatePost(@RequestBody Post post) {
         Post up = new Post.Builder().copy(post).build();
         return this.postService.update(up);
     }
 
-    @DeleteMapping("/ts/api/post/delete/{id}")
+    @DeleteMapping("/delete/{id}")
     public boolean deletePost(@PathVariable int id) {
         this.postService.delete(id);
         return this.postService.checkExists(id);
